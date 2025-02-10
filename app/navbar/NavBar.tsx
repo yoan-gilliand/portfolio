c05dfb7c5192876938dfdae23e8a514d08f734ec
Yoan Gilliand <yoangilliand@gmail.com>
2025-02-10 21:49:34 +0100

    Remove the PDF from the navbar

--- app/navbar/NavBar.tsx
+++ app/navbar/NavBar.tsx
@@ -19,21 +19,7 @@ const NavBar = () => {
 
   return (
     <nav className="fixed bottom-10 left-0 right-0 z-50 my-0  mx-auto  flex w-[306px] items-center justify-center gap-1 rounded-lg bg-[#07070a]/90 px-1 py-1 text-[#e4ded7] backdrop-blur-md sm:w-[383.3px] md:p-2 lg:w-[391.3px]">
-      <Link
-        href="https://drive.google.com/file/d/1Mmwl-FRGPm5QVe3AjtukJr_Jpc366Ear/view"
-        target="_blank"
-        className="flex"
-        aria-label="Open my resume"
-        data-blobity-tooltip="View Resume"
-        data-blobity-magnetic="false"
-      >
-        <FontAwesomeIcon
-          icon={faFilePdf}
-          className="py-2 px-2 text-[16px] sm:px-4 md:py-1"
-        />
-      </Link>
-
-      <Link
+        <Link
         href="#home"
         data-blobity-magnetic="false"
         onClick={handleScroll}
