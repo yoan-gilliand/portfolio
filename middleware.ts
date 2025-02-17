7140f1b317bca31c29f358435839dd8f6dc995c2
Yoan Gilliand <yoangilliand@gmail.com>
2025-02-17 14:32:52 +0100

    Update middleware's matcher

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,12 @@ import {routing} from './i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-    // Match only internationalized pathnames
-    matcher: ['/', '/(fr|en)/:path*']
+    matcher: [
+        // Match all pathnames except for
+        // - … if they start with `/api`, `/_next` or `/_vercel`
+        // - … the ones containing a dot (e.g. `favicon.ico`)
+        '/((?!api|_next|_vercel|.*\\..*).*)',
+        // However, match all pathnames within `/users`, optionally with a locale prefix
+        '/([\\w-]+)?/users/(.+)'
+    ]
 };
